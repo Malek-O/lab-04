76dc315f250ba197c08b4419f9be6abd85ad0abf
Malek-O <lslsls96996@gmail.com>
2023-05-05 08:28:19 +0300

    first commit

--- src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/Avatar.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/Avatar.java
@@ -2,6 +2,7 @@
 
 package sa.edu.kau.fcit.cpit252.lab4BuilderFactory;
 
+
 public final class Avatar {
 
   private final SkinTone skinTone;
@@ -10,29 +11,14 @@ public final class Avatar {
   private final BodyType bodyType;
   private final FacialFeatures facialFeatures;
 
-  public Avatar(SkinTone skinTone, HairType hairType, HairColor hairColor, BodyType bodyType, FacialFeatures facialFeatures) {
-    this.skinTone = skinTone;
-    this.hairType = hairType;
-    this.hairColor = hairColor;
-    this.bodyType = bodyType;
-    this.facialFeatures = facialFeatures;
+  private Avatar(Builder builder) {
+    this.skinTone = builder.skinTone;
+    this.hairColor = builder.hairColor;
+    this.hairType = builder.hairType;
+    this.bodyType = builder.bodyType;
+    this.facialFeatures = builder.facialFeatures;
   }
 
-  public Avatar(SkinTone skinTone, HairType hairType) {
-    this(skinTone, hairType, HairColor.BLACK, BodyType.FIT, FacialFeatures.CLEAN_SHAVEN);
-  }
-
-  public Avatar(SkinTone skinTone, HairType hairType, HairColor hairColor) {
-    this(skinTone, hairType, hairColor, BodyType.FIT, FacialFeatures.CLEAN_SHAVEN);
-  }
-
-  public Avatar(SkinTone skinTone, HairType hairType, HairColor hairColor, BodyType bodyType) {
-    this(skinTone, hairType, hairColor, bodyType, FacialFeatures.CLEAN_SHAVEN);
-  }
-
-  public Avatar(SkinTone skinTone,  BodyType bodyType) {
-    this(skinTone, HairType.SHORT, HairColor.BLACK, bodyType, FacialFeatures.CLEAN_SHAVEN);
-  }
 
   public SkinTone getSkinTone() {
     return skinTone;
@@ -53,6 +39,51 @@ public final class Avatar {
   public FacialFeatures getFacialFeatures() {
     return facialFeatures;
   }
+
+
+
+  public static class  Builder  {
+
+    private final SkinTone skinTone;
+    private  HairType hairType;
+    private  HairColor hairColor;
+    private  BodyType bodyType;
+    private  FacialFeatures facialFeatures;
+
+    public Builder (SkinTone skinTone){
+      if(skinTone == null){
+        throw new IllegalArgumentException("A skin tone is required");
+      }
+      this.skinTone = skinTone;
+    }
+
+    public Builder withHairType(HairType hairType){
+      this.hairType = hairType;
+      return this;
+    }
+    public Builder withHairColor(HairColor hairColor){
+      this.hairColor = hairColor;
+      return this;
+    }
+    public Builder withBodyType(BodyType bodyType){
+      this.bodyType = bodyType;
+      return this;
+    }
+    public Builder withFacialFeatures(FacialFeatures facialFeatures){
+      this.facialFeatures = facialFeatures;
+      return this;
+    }
+
+    public Avatar build(){
+      return new Avatar(this);
+    }
+
+
+  }
+
+
+
+
 
   @Override
   public String toString() {
